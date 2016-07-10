caf688a740a119db973bdac808a88d5057199869
MitulAP <mitulpatel.hsd@gmail.com>
2016-07-10 10:26:26 -0700

    CROS error fix

--- backend-app/app.js
+++ backend-app/app.js
@@ -11,9 +11,9 @@ mongoose.connect('mongodb://localhost/medair');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
 app.all(expressJWT({ secret: 'App Secret String'}). unless({ path: ['/users/login']}));
@@ -25,6 +25,12 @@ require('./app/routes/serviceRegistration')(app);
 require('./app/routes/organization')(app);
 require('./app/routes/serviceRequests')(app);
 
+
+app.all('/*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
+    next();
+});
 
 // app.get('*', function(req, res, next) {
 //   var err = new Error();
