e5a534592b85c992e8ff0a1c5eadca3b67a30dec
Pulkit <dureja.pulkit@gmail.com>
2016-07-10 04:19:30 -0700

    Latest Control Switch

--- Medair_v1/app/js/Controllers/CSwitch.js
+++ Medair_v1/app/js/Controllers/CSwitch.js
@@ -13,7 +13,11 @@ angular.module('CSwitch', ['ngRoute', 'ngCookies'])
                 .when('/Help', {
                     templateUrl: 'Help.html',
                     controller: "DashCtrl",
-                    // controllerAs: 'dash'
+                    controllerAs: 'dash'
+                })
+                .when('/pending/:name',{
+                    templateUrl:'ServicesPage.html',
+                    controller: "ReqCtrl"
                 })
                 // .otherwise({
                 //     templateUrl: ''
@@ -21,9 +25,146 @@ angular.module('CSwitch', ['ngRoute', 'ngCookies'])
 
             // $locationProvider.html5Mode(true);
         }])
+    .factory('restCall',['$http', function ($http) {
+        var requests;
+        requests=[{
+            "serviceId":"111123",
+            "RefugeeId":"reg11",
+            "dataAdded":"111123",
+            "status":"pending",
+            "comments":"test",
+            "reqId":"12312312312"
+        },
+            {
+                "serviceId":"222222",
+                "RefugeeId":"reg11",
+                "dataAdded":"111123",
+                "status":"pending",
+                "comments":"test",
+                "reqId":"12312312312"
+            },
+            {
+                "serviceId":"3333333",
+                "RefugeeId":"reg11",
+                "dataAdded":"111123",
+                "status":"pending",
+                "comments":"test",
+                "reqId":"12312312312"
+            },
+            {
+                "serviceId":"4444444",
+                "RefugeeId":"reg11",
+                "dataAdded":"111123",
+                "status":"pending",
+                "comments":"test",
+                "reqId":"12312312312"
+            },{
+                "serviceId":"5555555",
+                "RefugeeId":"reg11",
+                "dataAdded":"111123",
+                "status":"processing",
+                "comments":"test",
+                "reqId":"12312312312"
+            },{
+                "serviceId":"111123",
+                "RefugeeId":"reg11",
+                "dataAdded":"111123",
+                "status":"pending",
+                "comments":"test",
+                "reqId":"12312312312"
+            },{
+                "serviceId":"111123",
+                "RefugeeId":"reg11",
+                "dataAdded":"111123",
+                "status":"completed",
+                "comments":"test",
+                "reqId":"12312312312"
+            },{
+                "serviceId":"111123",
+                "RefugeeId":"reg11",
+                "dataAdded":"111123",
+                "status":"pending",
+                "comments":"test",
+                "reqId":"12312312312"
+            },{
+                "serviceId":"111123",
+                "RefugeeId":"reg11",
+                "dataAdded":"111123",
+                "status":"processing",
+                "comments":"test",
+                "reqId":"12312312312"
+            },{
+                "serviceId":"111123",
+                "RefugeeId":"reg11",
+                "dataAdded":"111123",
+                "status":"processing",
+                "comments":"test",
+                "reqId":"12312312312"
+            },{
+                "serviceId":"111123",
+                "RefugeeId":"reg11",
+                "dataAdded":"111123",
+                "status":"pending",
+                "comments":"test",
+                "reqId":"12312312312"
+            }
+            ];/*
+            $http({
+            method:"GET",
+            url:'https://www.google.com',
+            headers: undefined,
+            data:{
+                //JSON DATA to be sent Token ReqId
+            }
+        }).then(function(resp){
+            //resp.data contains all the JSON data.
+            //merge the resp.data array with a ng-model.
+            requests= resp.data;
 
+        }), function (resp) {
+            console.log("")
+        }*/
+        return requests;
+    }])
     //for loading component
+    .filter('unique', function () {
 
+        return function (items, filterOn) {
+
+            if (filterOn === false) {
+                return items;
+            }
+
+            if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+                var hashCheck = {}, newItems = [];
+
+                var extractValueToCompare = function (item) {
+                    if (angular.isObject(item) && angular.isString(filterOn)) {
+                        return item[filterOn];
+                    } else {
+                        return item;
+                    }
+                };
+
+                angular.forEach(items, function (item) {
+                    var valueToCheck, isDuplicate = false;
+
+                    for (var i = 0; i < newItems.length; i++) {
+                        if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+                            isDuplicate = true;
+                            break;
+                        }
+                    }
+                    if (!isDuplicate) {
+                        newItems.push(item);
+                    }
+
+                });
+                items = newItems;
+            }
+            return items;
+        };
+    })
     .directive('ifLoading', ['$http', function ($http) {
         return {
             restrict: 'A',
@@ -48,23 +189,20 @@ angular.module('CSwitch', ['ngRoute', 'ngCookies'])
             }
         };
     }])
-    .controller('DashCtrl',['$http','$scope',function($http,$scope){
-
-        $http({
-            method:"GET",
-            url:'https://www.google.com',
-            headers: undefined,
-            data:{
-                //JSON DATA to be sent Token ReqId
-            }
-        }).then(function(resp){
-            //resp.data contains all the JSON data.
-            //merge the resp.data array with a ng-model.
-            $scope.requests= resp.data;
-
-        }), function (resp) {
-            console.log("")
+    .controller('DashCtrl',['restCall','$http','$scope',function(restCall,$http,$scope){
+        $scope.requests=restCall;
+        $scope.accept=function () {
+         $scope.acptBtn ="Accepted";
         }
+            
+            
+
+    }])
+    .controller('ReqCtrl',['$scope','$routeParams','restCall',function ($scope,$routeParams,restCall) {
+        $scope.requests=restCall;
+        console.log("params :"+$routeParams.name);
+        $scope.serviceName=$routeParams.name;
+        // $scope.serviceName =
     }])
     //Directive for making alerts
     /*
